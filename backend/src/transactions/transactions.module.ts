41e31354dd4418b228f89a4641f81ed0d04c8ea3
aslandoganbekirmert-cyber <aslandoganbekirmert@gmail.com>
2026-02-09 01:02:40 +0300

    Update transactions.module.ts

--- backend/src/transactions/transactions.module.ts
+++ backend/src/transactions/transactions.module.ts
@@ -6,10 +6,12 @@ import { TransactionsService } from './transactions.service';
 import { TransactionsController } from './transactions.controller';
 import { SheetsService } from '../sheets/sheets.service';
 import { SheetsModule } from '../sheets/sheets.module';
+import { ProjectsModule } from '../projects/projects.module';
 
 @Module({
     imports: [
         TypeOrmModule.forFeature([MaterialTransaction]),
+        ProjectsModule, // ProjectsService için gerekli
         // SheetsModule is @Global(), no need to import it here
     ],
     controllers: [TransactionsController],
