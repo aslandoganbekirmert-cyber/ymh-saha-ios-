115305fdeba9f0c99635db5fc6b3f4a01e921c28
aslandoganbekirmert-cyber <aslandoganbekirmert@gmail.com>
2026-02-08 23:07:34 +0300

    Update projects.module.ts

--- backend/src/projects/projects.module.ts
+++ backend/src/projects/projects.module.ts
@@ -3,9 +3,13 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { ProjectsService } from './projects.service';
 import { ProjectsController } from './projects.controller';
 import { Project } from './project.entity';
+import { SheetsModule } from '../sheets/sheets.module';
 
 @Module({
-    imports: [TypeOrmModule.forFeature([Project])],
+    imports: [
+        TypeOrmModule.forFeature([Project]),
+        SheetsModule
+    ],
     controllers: [ProjectsController],
     providers: [ProjectsService],
     exports: [ProjectsService],
