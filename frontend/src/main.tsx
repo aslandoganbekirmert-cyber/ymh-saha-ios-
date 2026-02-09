35601fecdd737bcb8a92961b53eba0d5b8db3196
aslandoganbekirmert-cyber <aslandoganbekirmert@gmail.com>
2026-02-09 09:14:49 +0300

    Update main.tsx

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,7 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
-import './index.css'
+import { defineCustomElements } from '@ionic/pwa-elements/loader';
+
+// Web ortamında kamera vb. pluginlerin çalışması için gerekli
+defineCustomElements(window);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
